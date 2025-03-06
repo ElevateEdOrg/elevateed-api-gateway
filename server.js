31f3e370a0a21267f9f9c66d569999982c9d1228
PremSingh68749 <prems.wappnet@gmail.com>
2025-03-06 11:42:37 +0530

    proudction changes

--- server.js
+++ server.js
@@ -3,8 +3,7 @@ const { createProxyMiddleware } = require("http-proxy-middleware");
 const path = require("path");
 const dotenv = require("dotenv");
 const cors = require("cors");
-const fs = require("fs");
-const https = require("https");
+
 
 dotenv.config(); // Load environment variables
 
@@ -35,11 +34,7 @@ app.all("*", (req, res, next) => {
   next();
 });
 
-// Redirect HTTPS (443) requests to HTTP (80)
-const httpsApp = express();
-httpsApp.use((req, res) => {
-  res.redirect(`http://${req.headers.host}${req.url}`);
-});
+
 
 // Proxy routes
 app.use(
@@ -83,18 +78,8 @@ app.get("*", (req, res) => {
 });
 
 // Start the HTTP server on port 80
-const httpPort = process.env.API_GATEWAY_PORT || 80;
-app.listen(httpPort, "0.0.0.0", () => {
+
+app.listen(80, "0.0.0.0", () => {
   console.log(`API Gateway is running on port ${httpPort}`);
 });
 
-// Start the HTTPS server on port 443 with a self-signed SSL certificate
-const httpsPort = 443;
-const sslOptions = {
-  key: fs.readFileSync("/etc/ssl/private/server.key"),
-  cert: fs.readFileSync("/etc/ssl/certs/server.crt"),
-};
-
-https.createServer(sslOptions, httpsApp).listen(httpsPort, () => {
-  console.log(`HTTPS redirect server is running on port ${httpsPort}`);
-});
